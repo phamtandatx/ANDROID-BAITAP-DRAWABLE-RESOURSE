c52fffbdfb1913471d0828b2fece0a2ca0a3fc7b
phamtandatx <phamtandatx@gmail.com>
2020-04-26 21:42:28 +0700

    ex 1 finish

--- app/src/main/java/com/example/baitap_drawable_resourse/MainActivity.java
+++ app/src/main/java/com/example/baitap_drawable_resourse/MainActivity.java
@@ -8,12 +8,18 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Date;
 
-    @Override
+public class MainActivity extends AppCompatActivity {
+    TextView textView4;
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String minutes = DateFormat.getDateTimeInstance().format(new Date());
+////        textView4.setText(minutes);
     }
 }
