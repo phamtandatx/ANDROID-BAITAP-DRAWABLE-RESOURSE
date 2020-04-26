2a99f02d0b88deb5f272b3f3d58e082810c0f2a1
phamtandatx <phamtandatx@gmail.com>
2020-04-26 08:01:21 +0700

    bai 1

---
+++ app/src/main/java/com/example/baitap_drawable_resourse/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.baitap_drawable_resourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
