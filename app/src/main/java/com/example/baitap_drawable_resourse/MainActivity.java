af8920e2bafd07fcf4f0394986ebb339f67ef685
phamtandatx <phamtandatx@gmail.com>
2020-05-02 15:53:26 +0700

    clean code

--- app/src/main/java/com/example/baitap_drawable_resourse/MainActivity.java
+++ app/src/main/java/com/example/baitap_drawable_resourse/MainActivity.java
@@ -4,43 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //    TextView Tvtext ;
-//    Button mbtn;
+
     TextView txtRating;
     RatingBar ratingBar;
+    public String string_value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Tvtext = findViewById(R.id.textView);
-//        mbtn = findViewById(R.id.button);
-//
-//        mbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Tvtext.setPaintFlags(Tvtext.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-//            }
-//        });
-        txtRating = (TextView)findViewById(R.id.text_rating);
-        ratingBar = (RatingBar)findViewById(R.id.ratingBar_custom);
+        txtRating = (TextView) findViewById(R.id.text_rating);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar_custom);
 
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-
-                txtRating.setText(rating +"/5");
+                string_value = (String.valueOf(rating));
+                String num_star = "5";
+                String sourceString = "<big>" + "<b>" + string_value + "</b> " + "</big>" + "/" + num_star;
+                txtRating.setText(Html.fromHtml(sourceString));
             }
         });
-
-
-
     }
 
 }
